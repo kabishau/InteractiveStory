5b98e91cea262e33e980fdb7cd8c8940d513f738
AlekseyK <aleksey.kabishau@gmail.com>
2018-11-12 23:14:37 -0500

    Initial Commit

---
+++ InteractiveStory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by Aleksey on 1112..18.
+//  Copyright © 2018 Aleksey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
